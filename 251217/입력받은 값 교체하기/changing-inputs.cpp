7b85cbb644b3956fa0f6d679b72f0672d347efa4
triky <lp207086@naver.com>
2025-12-17 15:32:06 +0900

    Title: 입력받은 값 교체하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251217/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    1 <= a,b <= 100;
+    
+    cin >> a >> b;
+    cout << b << " " << a << "\n";
+    // Please write your code here.
+    return 0;
+}
