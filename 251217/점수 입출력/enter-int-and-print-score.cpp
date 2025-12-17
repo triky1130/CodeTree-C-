fb513f058b9eee9b109b93a5911b93b410bd81a5
triky <lp207086@naver.com>
2025-12-17 12:26:34 +0900

    Title: 점수 입출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251217/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    1 <= N <= 1000;
+    cin >> N;
+    cout << "Your score is " << N << " point." << "\n";
+    
+    // Please write your code here.
+    return 0;
+}
