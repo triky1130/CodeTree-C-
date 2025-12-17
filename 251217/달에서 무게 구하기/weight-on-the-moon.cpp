a95e285a1f41ffa71f560685c97feb23886dfb1e
triky <lp207086@naver.com>
2025-12-17 12:02:53 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251217/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int weight = 13;
+    double moon_ratio = 0.165;
+    cout << fixed;
+    cout.precision(6);
+
+    cout << weight << " * " << moon_ratio << " = " << weight * moon_ratio << "\n";
+    // Please write your code here.
+    return 0;
+}
