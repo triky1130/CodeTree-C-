20260edb269130768ad49b55d4383b4d5ec4dc7f
triky <lp207086@naver.com>
2025-12-17 15:47:53 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251217/합과 평균/sum-and-avg.cpp
+++ 251217/합과 평균/sum-and-avg.cpp
@@ -9,12 +9,7 @@ int main() {
     cin >> A >> B;
     cout << fixed;
     cout.precision(1);
-    
-    double a = (double)A;
-    double b = (double)B;
 
-    double total = a + b;
-    double avg = total / 2;
-    cout << A + B << " " << avg << "\n";// Please write your code here.
+    cout << A + B << " " << (double)(A + B) / 2 << "\n";// Please write your code here.
     return 0;
 }
