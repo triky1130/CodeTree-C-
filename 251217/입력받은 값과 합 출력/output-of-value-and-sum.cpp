5ff3cdfec95d01623932c230498d3d8ac33dec32
triky <lp207086@naver.com>
2025-12-17 15:34:17 +0900

    Title: 입력받은 값과 합 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251217/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    1 <= A,B <= 100;
+    cin >> A >> B;
+    
+    cout << A << " " << B << " " << A+B << "\n";
+    // Please write your code here.
+    return 0;
+}
