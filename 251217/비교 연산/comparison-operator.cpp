1750e4fa4a44569ed9f41844b063718177d7d127
triky <lp207086@naver.com>
2025-12-17 17:11:52 +0900

    Title: 비교 연산, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251217/비교 연산/comparison-operator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    1 <= A;
+    B <= 100;
+    cin >> A >> B;
+
+    cout<< (A >= B) << "\n";
+    cout<< (A > B) << "\n";
+    cout<< (B >= A) << "\n";
+    cout<< (B > A) << "\n";
+    cout<< (A == B) << "\n";
+    cout<< (A != B) << "\n";
+
+         // Please write your code here.
+    return 0;
+}
