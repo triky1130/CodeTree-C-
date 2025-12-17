1d56971e2dd5e595080aef839be5cc5246c7127f
triky <lp207086@naver.com>
2025-12-17 12:35:35 +0900

    Title: 실수 받아 그대로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251217/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double N;
+    0 <= N <= 50;
+    
+    cin >> N;
+    cout << fixed;
+    cout.precision(2);
+    
+    cout << N << "\n";
+    // Please write your code here.
+    return 0;
+}
