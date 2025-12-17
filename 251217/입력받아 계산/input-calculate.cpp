a49eabeb338984b0533327ff85f31901b8df9c12
triky <lp207086@naver.com>
2025-12-17 12:31:40 +0900

    Title: 입력받아 계산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251217/입력받아 계산/input-calculate.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    1 <= a <= 100;
+    
+    cin >> a;
+    cout << a+2 << "\n";
+    // Please write your code here.
+    return 0;
+}
