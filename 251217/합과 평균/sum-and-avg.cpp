29b09f6514bd47ddba706a1a673fc5c7be26528e
triky <lp207086@naver.com>
2025-12-17 15:45:08 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251217/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    1 <= A;
+    B <= 100;
+    
+    cin >> A >> B;
+    
+    double a = (double)A;
+    double b = (double)B;
+
+    double avg = (a + b) / 2;
+    cout << A + B << " " << avg << "\n";// Please write your code here.
+    return 0;
+}
