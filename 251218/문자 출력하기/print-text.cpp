3dc0a726b636221a8ff7ab28f1391c76ebaecf06
triky <lp207086@naver.com>
2025-12-18 11:19:06 +0900

    Title: 문자 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251218/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    int i;
+    cin >> a;
+    
+    for(i = 0; i < 8; i++){
+        cout << a;
+    }// Please write your code here.
+    return 0;
+}
