417067209259ddb15d9a0ad7273c4c65a55b24fb
triky <lp207086@naver.com>
2025-12-18 11:26:57 +0900

    Title: N번 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251218/N번 더하기/add-n-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, N, i;
+    1 <= A,N <= 10;
+    cin >> A >> N;
+
+    for(i = 0; i < N; i++){
+        A = A + N;
+        cout << A << "\n";
+    }
+    // Please write your code here.
+    return 0;
+}
