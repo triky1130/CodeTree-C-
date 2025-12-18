6586982487c7bbb7578865b5f757ba089314275e
triky <lp207086@naver.com>
2025-12-18 12:31:52 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251218/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, N;
+    1 <= N <= 100;
+    cin >> N;
+    
+    for(i = 1;i < 6; i++){
+        cout << N * i << " ";
+    } // Please write your code here.
+    return 0;
+}
