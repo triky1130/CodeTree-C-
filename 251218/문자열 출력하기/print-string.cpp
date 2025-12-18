ed52a092e70a21ca57cb9152927bcad24c8a8836
triky <lp207086@naver.com>
2025-12-18 11:22:39 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251218/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, N;
+    1 <= N <= 10;
+    cin >> N;
+    
+    for(i = 0; i < N; i++){
+        cout << "LeebrosCode\n";
+    }// Please write your code here.
+    return 0;
+}
