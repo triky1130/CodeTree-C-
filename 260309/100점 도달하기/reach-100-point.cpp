6cb248ea5069ef26ac152b5497bb5da651dc51ed
triky <lp207086@naver.com>
2026-03-09 22:12:23 +0900

    Title: 100점 도달하기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 260309/100점 도달하기/reach-100-point.cpp
+++ 260309/100점 도달하기/reach-100-point.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int score;
-    cin >> score;
+    int n;
 
-    for(int i = score; i <= 100; i++){
-        if(i >= 90)
-            cout << "A" << " ";
-        else if(i >= 80)
-            cout << "B" << " ";
-        else if(i >= 70)
-            cout << "C" << " ";
-        else if(I >= 60)
-            cout << "D" << " ";
-        else
-            cout << "F" << " ";
-    }
+	cin >> n;
+    
+    // 출력
+	for(int i = n; i <= 100; i++) {
+		if(i >= 90)
+			cout << "A" << " ";
+		else if(i >= 80)
+			cout << "B" << " ";
+		else if(i >= 70)
+			cout << "C" << " ";
+		else if(i >= 60)
+			cout << "D" << " ";
+		else
+			cout << "F" << " ";
+	}
+
     return 0;
 }
